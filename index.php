334290c879204e0760afa6cf3d0ab0b0ea69311a
Silvia-Du <silviadu@hotmail.it>
2022-06-17 15:30:09 +0200

    Primo commit: impostazione base di lavoro

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+
+/*
+- è definita una classe ‘Movie’
+   => all’interno della classe sono dichiarate delle variabili d’istanza
+   => all’interno della classe è definito un costruttore
+   => all’interno della classe è definito almeno un metodo
+- vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà
+La classe può essere definita internamente al file index.php o essere inclusa (soluzione preferibile)
+*/
+
+require_once __DIR__. './partial/head.php';
+require_once __DIR__. './partial/header.php';
+
+?>
+
---
+++ partial/footer.php
@@ -0,0 +1,3 @@
+<footer>F</footer>
+</body>
+</html>
